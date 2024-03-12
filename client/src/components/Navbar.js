1ac8e6d6f7f18b11a601843f52e4bdee85bac407
ishwargoswami <kanoguru99@gmail.com>
2024-03-13 00:02:58 +0530

    navbar added

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar(){
+    <div className="navbar">
+        <div className="logo">
+            <h4>MentorMe</h4>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+    
+}
+
+export default Navbar;
